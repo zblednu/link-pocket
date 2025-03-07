b80adea8eb19d42ef8db6086c083b9ec960d9ae9
zblednu <maksym.surovtsev@student.tuke.sk>
2025-03-07 22:22:18 +0100

    make json middleware global

---
+++ src/services/auth.ts
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken'
+
+export function authenticateByCredentials() {
+
+}
+
+export function createToken(payload: object) {
+
+}
+
+export function authenticateByToken() {
+
+}
